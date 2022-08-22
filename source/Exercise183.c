2573146cb14e07148afd80abf108d2ecf915a4c2
thanhngan22 <nganbui9a2017@gmail.com>
2022-08-23 01:47:45 +0700

    :sob: fix from ex181 - ex184

--- source/Exercise183.c
+++ source/Exercise183.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <string.h>
 
@@ -27,38 +26,6 @@ int main()
     }
     reverse(file1,file2);
     printf("Hoan thanh...\n");
-    _fcloseall;
+    _fcloseall();
     return 225;
-=======
-#include <stdio.h>
-#include <string.h>
-
-void reverse(FILE *file1,FILE *file2)
-{
-    char s[255];
-    if (fgets(s,255,file1)==NULL)
-    {
-        return;
-    }
-    reverse(file1,file2);
-    fprintf(file2,"%s",s);
-}
-
-
-int main()
-{
-    FILE *file1=NULL;
-    FILE *file2=NULL;
-    file1=fopen("thanhnganinp.txt","r");
-    file2=fopen("thanhnganout.txt","w");
-    while (!file1 || !file2)
-    {
-        printf("That bai :<\n");
-        return 225;
-    }
-    reverse(file1,file2);
-    printf("Hoan thanh...\n");
-    _fcloseall;
-    return 225;
->>>>>>> ff8bb0a6669dac9096cb2b6720d81446bdb396f9
 }
--- source/Exercise184.c
+++ source/Exercise184.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <string.h>
 #define MAX 100
@@ -26,33 +25,4 @@ int main()
 
 
     return 225;
-=======
-#include <stdio.h>
-#include <string.h>
-#define MAX 100
-
-int main()
-{
-    FILE *file=NULL;
-    file=fopen("EMP.DAT","w");
-    while (!file)
-    {
-        printf("That bai :<\n");
-        return 225;
-    }
-    printf("Nhap so nhan vien: ");
-    int sl;
-    scanf("%d",&sl);
-    printf("Nhap (ten, ngay, thang, nam sinh, luong) :\n");
-    for (int i=1;i<=sl;i++)
-    {
-
-    }
-    printf("Nhap du lieu xong...\n");
-    printf("Xuat danh sach sap xep:\n");
-
-
-
-    return 225;
->>>>>>> ff8bb0a6669dac9096cb2b6720d81446bdb396f9
 }

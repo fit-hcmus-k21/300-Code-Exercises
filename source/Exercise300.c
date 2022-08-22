08841a185cfe590a6b2ffd1a7187c8462db1dd30
thanhngan22 <nganbui9a2017@gmail.com>
2022-08-23 01:51:17 +0700

    :sob: finished fix err caused by set upstream from ex293 - ex300 :cry:

--- source/Exercise300.c
+++ source/Exercise300.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <string.h>
 #define MAX 100
@@ -62,69 +61,4 @@ int main()
     printf("\n");
 
     return 225;
-=======
-#include <stdio.h>
-#include <string.h>
-#define MAX 100
-void hoandoi(int *a,int *b)
-{
-    int temp=*a;
-    *a=*b;
-    *b=temp;
-}
-
-void sapxep(int a[],int n)
-{
-    for (int i=0;i<n-1;i++)
-    {
-        for (int j=i+1;j<n;j++)
-        {
-            if (a[j]>a[i])
-            {
-                hoandoi(&a[i],&a[j]);
-            }
-        }
-    }
-}
-
-void in(int a[],int n)
-{
-    printf(" %d ",a[0]);
-    for (int i=1;i<n;i++)
-    {
-        if (a[i]!=a[i-1])
-        {
-            printf("  %d  ",a[i]);
-        }
-    }
-}
-
-void xuat(int a[],int n)
-{
-    printf("\nSAP XEP : ");
-    for (int i=0;i<n;i++)
-    {
-        printf(" %d ",a[i]);
-    }
-}
-
-int main()
-{
-    int a[MAX];
-    int n;
-    printf("Nhap n:");
-    scanf("%d",&n);
-    printf("Nhap mang:\n");
-    for (int i=0;i<n;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    sapxep(a,n);
-    xuat(a,n);
-    printf("\n");
-    in(a,n);
-    printf("\n");
-
-    return 225;
->>>>>>> ff8bb0a6669dac9096cb2b6720d81446bdb396f9
 }
